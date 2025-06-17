37db9aa220615ce8be0956d81e6bc911c02642db
GARUD6209 <harshvardhachandravanshi@gmail.com>
2025-06-17 17:36:52 +0530

    refactor AdminModule to include user management components and enhance module imports

--- src/app/features/admin/admin.module.ts
+++ src/app/features/admin/admin.module.ts
@@ -5,15 +5,36 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component';
 import { AdminComponent } from './pages/admin-dashboard/admin.component';
 
+import { ViewUserComponent } from './components/view-user/view-user.component';
+import { FormsModule } from '@angular/forms';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { AddUserComponent, SimpleDialogComponent } from './components/add-user/add-user.component';
+
 
 @NgModule({
   declarations: [
     AdminDashboardComponent,
-    AdminComponent
+    AdminComponent,
+    AddUserComponent,
+    ViewUserComponent,
+    SimpleDialogComponent
   ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDialogModule
   ]
 })
 export class AdminModule { }
