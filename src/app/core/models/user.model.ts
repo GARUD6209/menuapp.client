45b8772653c472589bfaa25dcd1a7e11d046bb8c
GARUD6209 <harshvardhachandravanshi@gmail.com>
2025-06-17 17:21:15 +0530

    add user model interfaces for login and registration

---
+++ src/app/core/models/user.model.ts
@@ -0,0 +1,17 @@
+export interface LoginRequest {
+    username: string;
+    password: string;
+}
+
+export interface LoginResponse {
+    id: number;
+    username: string;
+    role: string;
+    token: string;
+}
+
+export interface RegisterRequest {
+    username: string;
+    password: string;
+    role: number;
+}
