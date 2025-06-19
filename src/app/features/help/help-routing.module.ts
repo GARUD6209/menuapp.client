ad85bb2c174864cdb87bc5396c6c709aefdec8b6
GARUD6209 <harshvardhachandravanshi@gmail.com>
2025-06-19 16:08:19 +0530

    feat: add routing for reset password component with breadcrumb data

--- src/app/features/help/help-routing.module.ts
+++ src/app/features/help/help-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'reset-password',
+    component: ResetPasswordComponent,
+    data: { breadcrumb: 'reset' }
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
