5164841aec693a90aadefcc89147524830ff2ac0
GARUD6209 <harshvardhachandravanshi@gmail.com>
2025-06-19 16:08:51 +0530

    feat: add ResetPasswordComponent to HelpModule declarations and update imports

--- src/app/features/help/help.module.ts
+++ src/app/features/help/help.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
-import { HelpRoutingModule } from './help-routing.module';
 import { FaqComponent } from './pages/faq/faq.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
+import { SharedModule } from '../../shared/shared.module';
+import { HelpRoutingModule } from './help-routing.module';
+
+
 
 
 @NgModule({
   declarations: [
     FaqComponent,
-    ContactComponent
+    ContactComponent,
+    ResetPasswordComponent
+
   ],
   imports: [
-    CommonModule,
+    SharedModule,
     HelpRoutingModule
   ]
 })
